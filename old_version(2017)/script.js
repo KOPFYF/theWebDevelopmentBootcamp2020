e62babae9c5606843e63981835cfe9e586d82be9
KOPFYF <fei8@purdue.edu>
2020-10-13 23:16:25 -0400

    1st

---
+++ old_version(2017)/script.js
@@ -0,0 +1,7 @@
+var name = prompt("what's your name");
+alert("hello from js, welcome " + name);
+console.log("hello from js, welcome " + name);
+
+people = [{name: 'Angela', age: 26},
+          {name: 'Bob', age: 34}, 
+          {name: 'Carb', age: 26}]
